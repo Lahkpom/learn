fd6704a23d306e28592d8a3557801b9fe810e58f
Lahkpom <118.hidalgo.leonel.1358@gmail.com>
2023-10-09 19:41:09 -0300

    more dart

--- MOVIL_DEV/Dart/3_2_anonymous_function/main.dart
+++ MOVIL_DEV/Dart/3_2_anonymous_function/main.dart
@@ -1,1 +1,13 @@
-void main() {}
+void main() {
+  //*Funciones sin nombre
+  void functionA() {
+    print('This is a function');
+  }
+
+  void functionB(Function myFunction) {
+    myFunction();
+    print('This is b function');
+  }
+
+  functionB(functionA);
+}
