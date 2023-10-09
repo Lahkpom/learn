9b8f2c1f5d95cbd6c32f1664f0f54ad8f853fe6c
Lahkpom <118.hidalgo.leonel.1358@gmail.com>
2023-10-09 14:51:31 -0300

    more dart

--- MOVIL_DEV/Dart/3_1_Functions_records/main.dart
+++ MOVIL_DEV/Dart/3_1_Functions_records/main.dart
@@ -1,1 +1,32 @@
-void main() {}
+void main() {
+  //*variables
+  int result;
+
+  //*functions
+  void sum(int a, int b) {
+    print('Result = ${a + b}');
+  }
+
+  sum(5, 5);
+
+  int sum2(int a, int b) {
+    return a + b;
+  }
+
+  result = sum2(6, 6);
+
+  print('Result = ${result}');
+
+  //*records (retorna mas de u valor)
+  (int s, int m) sum_multiply(int a, int b) {
+    return (a + b, a * b);
+  }
+
+  //*Si no pongo final no me deja
+  final result4 = sum_multiply(5, 5);
+  print('sum_multiply = $result4');
+
+  final (resul2, result3) = sum_multiply(5, 5);
+  print('Sum = $resul2');
+  print('Multiply = $result3');
+}
