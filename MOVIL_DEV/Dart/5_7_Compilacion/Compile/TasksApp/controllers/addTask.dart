66bf91ff6f55ba4c0a42dafba236f488e6d4f828
Lahkpom <118.hidalgo.leonel.1358@gmail.com>
2023-11-20 17:13:14 -0300

    more git

--- MOVIL_DEV/Dart/5_7_Compilacion/Compile/TasksApp/controllers/addTask.dart
+++ MOVIL_DEV/Dart/5_7_Compilacion/Compile/TasksApp/controllers/addTask.dart
@@ -20,7 +20,7 @@ void addTask(File file){
       Task newTask = Task(name, description: description);
 
       file.writeAsStringSync(content + '\n' + newTask.toString()); 
-      
+
     }else{
       throw new Exception("File doesn't exist");
     }
